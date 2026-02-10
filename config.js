41d741aa77def5ede9bcc74cd86ba4521ecc9ce1
companyinfinity <companyinfinity@gmail.com>
2026-02-10 15:33:17 +0100

    migrate (pusher => socket.io)

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     BACKEND_URL: 'http://localhost:8085',
-    PUSHER_KEY: 'f02185b1bc94c884ce5b',
-    PUSHER_CLUSTER: 'eu',
+    SOCKETIO_URL: 'http://localhost:3001',
     DEBUG_MODE: true
 };
