ba800fccaea1eea20e107a853bf7db7658f03ac8
companyinfinity <companyinfinity@gmail.com>
2026-03-11 14:58:25 +0100

    install workerman

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ module.exports = {
     BACKEND_URL: 'http://localhost:8085',
     REVERB_APP_KEY: 'game-key',
     REVERB_HOST: 'localhost',
-    REVERB_PORT: 8080,
+    REVERB_PORT: 8081,
     REVERB_SCHEME: 'http',
     REVERB_CLUSTER: 'mt1',
+    DRAW_WS_URL: 'ws://localhost:8080',
     DEBUG_MODE: true
 };
