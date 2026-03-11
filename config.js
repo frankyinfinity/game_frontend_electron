1a5a0643265eb0f06482acaf9c99551e020fab8b
companyinfinity <companyinfinity@gmail.com>
2026-03-11 14:33:08 +0100

    install reverb

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     BACKEND_URL: 'http://localhost:8085',
-    SOCKETIO_URL: 'http://localhost:3001',
+    REVERB_APP_KEY: 'game-key',
+    REVERB_HOST: 'localhost',
+    REVERB_PORT: 8080,
+    REVERB_SCHEME: 'http',
+    REVERB_CLUSTER: 'mt1',
     DEBUG_MODE: true
 };
